8bf6a6eb11e9b614ede7deaf938b5e4c9623bbbf
Kaio Herculano <kaioherculano12@gmail.com>
2026-07-13 17:04:54 -0400

    feat: add global timer widget and fix user profile creation

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class UserProfile(models.Model):
@@ -22,3 +24,14 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return f"Perfil de {self.user.username}"
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
